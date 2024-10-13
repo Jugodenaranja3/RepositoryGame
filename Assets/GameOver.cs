d5cefaf4c3d726c4a89399ff5d52375b78f5efc3
jugodenaranja3 <vrr0032855@est.univalle.edu>
2024-10-13 07:13:41 -0400

    Intefaz Menu Inicial y Final, Interfaz Juego

---
+++ Assets/GameOver.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Reiniciar()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void MenuIncial(string nombre)
+    {
+        SceneManager.LoadScene(nombre);
+    }
+    public void Salir()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+}
